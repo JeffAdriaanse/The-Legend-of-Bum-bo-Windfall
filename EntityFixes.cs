c2e034ebe9a7f9e52be0a915f5f6a232f29b66f8
Jeff <jeff.adriaanse@gmail.com>
2021-09-22 20:38:43 -0700

    First commit

---
+++ EntityFixes.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using System.Reflection.Emit;
+
+namespace The_Legend_of_Bum_bo_Windfall
+{
+    class EntityFixes
+    {
+        public static void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(EntityFixes));
+            Console.WriteLine("[The Legend of Bum-bo: Windfall] Applying entity related bug fixes");
+        }
+
+        //Patch: Fixes a bug that caused invincible enemies to make the boss health bar display their healh amount when healed; the invincible enemy will now not be healed instead
+        //Patch also causes the healing 'gulp' sound to not play for the invincible enemy
+        [HarmonyPrefix, HarmonyPatch(typeof(Enemy), "AddHealth")]
+        static bool Enemy_AddHealth(Enemy __instance)
+        {
+            if (__instance.max_health == Enemy.HealthType.INVINCIBLE)
+            {
+                Console.WriteLine("[The Legend of Bum-bo: Windfall] Aborting healing of " + __instance.enemyName + "; enemy is invincible");
+                return false;
+            }
+            return true;
+        }
+
+        //Patch: Fixes champion enemies creating dust twice when spawning
+        [HarmonyPrefix, HarmonyPatch(typeof(Enemy), "SpawnDust")]
+        static bool Enemy_SpawnDust(Enemy __instance)
+        {
+            if (__instance.championType == Enemy.ChampionType.NotAChampion)
+            {
+                return true;
+            }
+            Console.WriteLine("[The Legend of Bum-bo: Windfall] Aborting enemy dust spawn; enemy is a champion");
+            return false;
+        }
+
+        //Patch: Fixes status effects being invisible on the eyes of Peeper/Tainted Peeper
+        [HarmonyPostfix, HarmonyPatch(typeof(PeepEyeEnemy), "Init")]
+        static void PeepEyeEnemy_Init(PeepEyeEnemy __instance)
+        {
+            __instance.poisonView.transform.Rotate(0f, 180f, 0f);
+            __instance.poisonView.transform.Translate(0f, 0f, -0.08f);
+            __instance.blindObject.transform.Rotate(0f, 180f, 0f);
+            __instance.blindObject.transform.Translate(0f, 0f, -0.08f);
+            Console.WriteLine("[The Legend of Bum-bo: Windfall] Rotating status effects of " + __instance.enemyName);
+        }
+
+        //Patch: Fixes TadoEnemy init overriding Greedling movement, which would cause Greedlings to have only one movement
+        [HarmonyPostfix, HarmonyPatch(typeof(TadoEnemy), "Init")]
+        static void TadoEnemy_Init(TadoEnemy __instance)
+        {
+            if (__instance.enemyName == EnemyName.Greedling)
+            {
+                __instance.turns = 2;
+                Console.WriteLine("[The Legend of Bum-bo: Windfall] Overriding TadoEnemy turns amount on init; Greedling now has two movement");
+            }
+        }
+
+        //***************************************************
+        //*************Blobby enemy health states************
+        //***************************************************
+        //These patches fix Blobby type enemies spawning with incorrect health state if enemies of the same type have already been defeated in previous rooms of the current chapter
+
+        //Patch: Forces Blobby health state to be of type full on init
+        [HarmonyPostfix, HarmonyPatch(typeof(GreenBlobbyEnemy), "Init")]
+        static void GreenBlobbyEnemy_Init(GreenBlobbyEnemy __instance)
+        {
+            if (__instance.healthState == GreenBlobbyEnemy.HealthState.half)
+            {
+                __instance.healthState = GreenBlobbyEnemy.HealthState.full;
+                Console.WriteLine("[The Legend of Bum-bo: Windfall] Overriding starting health state of newly spawned Blobby");
+            }
+        }
+
+        //Patch: Forces Red Blobby health state to be of type full on init
+        [HarmonyPostfix, HarmonyPatch(typeof(RedBlobbyEnemy), "Init")]
+        static void RedBlobbyEnemy_Init(RedBlobbyEnemy __instance)
+        {
+            if (__instance.healthState == RedBlobbyEnemy.HealthState.half)
+            {
+                __instance.healthState = RedBlobbyEnemy.HealthState.full;
+                Console.WriteLine("[The Legend of Bum-bo: Windfall] Overriding starting health state of newly spawned Red Blobby");
+            }
+        }
+
+        //Patch: Forces Black Blobby health state to be of type full on init
+        [HarmonyPostfix, HarmonyPatch(typeof(BlackBlobbyEnemy), "Init")]
+        static void BlackBlobbyEnemy_Init(BlackBlobbyEnemy __instance)
+        {
+            if (__instance.healthState == BlackBlobbyEnemy.HealthState.half)
+            {
+                __instance.healthState = BlackBlobbyEnemy.HealthState.full;
+                Console.WriteLine("[The Legend of Bum-bo: Windfall] Overriding starting health state of newly spawned Black Blobby");
+            }
+        }
+        //***************************************************
+        //***************************************************
+        //***************************************************
+
+        //***************************************************
+        //*****************Green fog fixes*******************
+        //***************************************************
+        //These patches cause the game to check for triggered fog more often, preventing triggered fog from lingering
+
+        //Patch: Changing AttackFlyEvent such that it checks for countering enemies when all AttackFlyEvents are finished; consequently, remaining green fog will always be triggered at the end of the enemy phase
+        [HarmonyPostfix, HarmonyPatch(typeof(AttackFlyEvent), "NextEvent")]
+        static void AttackFlyEvent_NextEvent(AttackFlyEvent __instance, ref BumboEvent __result)
+        {
+            if (__result.ToString() == "NewRoundEvent" && (__instance.app.model.isEnemyCountering || __instance.app.model.isFogCountering))
+            {
+                __result = new MonsterCounterEvent(new NewRoundEvent());
+                Console.WriteLine("[Bum-bo Update Mod] Changing AttackFlyEvent return event to MonsterCounterEvent");
+            }
+        }
+
+        //Patch: Changing BoneMegaAttackEvent such that it checks for countering enemies
+        [HarmonyPostfix, HarmonyPatch(typeof(BoneMegaAttackEvent), "NextEvent")]
+        static void BoneMegaAttackEvent_NextEvent(BoneMegaAttackEvent __instance, ref BumboEvent __result)
+        {
+            if (__result.ToString() == "NextComboEvent" && (__instance.app.model.isEnemyCountering || __instance.app.model.isFogCountering))
+            {
+                __result = new MonsterCounterEvent(new NextComboEvent());
+                Console.WriteLine("[Bum-bo Update Mod] Changing BoneMegaAttackEvent return event to MonsterCounterEvent");
+            }
+        }
+
+        //Patch: Changing ToothMegaAttackEvent such that it checks for countering enemies
+        [HarmonyPostfix, HarmonyPatch(typeof(ToothMegaAttackEvent), "NextEvent")]
+        static void ToothMegaAttackEvent_NextEvent(ToothMegaAttackEvent __instance, ref BumboEvent __result)
+        {
+            if (__result.ToString() == "NextComboEvent" && (__instance.app.model.isEnemyCountering || __instance.app.model.isFogCountering))
+            {
+                __result = new MonsterCounterEvent(new NextComboEvent());
+                Console.WriteLine("[Bum-bo Update Mod] Changing ToothMegaAttackEvent return event to MonsterCounterEvent");
+            }
+        }
+        //***************************************************
+        //***************************************************
+        //***************************************************
+
+        //Patch: Fixes enemies spawning with incorrect health amounts if enemies of the same type have already been defeated in previous rooms of the current chapter
+        //This patch overrides enemy starting health values and uses hard coded health instead 
+        [HarmonyPostfix, HarmonyPatch(typeof(Enemy), "Spawn")]
+        static void Enemy_Spawn(Enemy __instance)
+        {
+            int newHealthValue = 0;
+            switch (__instance.app.model.enemies[__instance.app.model.enemies.Count - 1].enemyName)
+            {
+                case EnemyName.Arsemouth:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.Butthead:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.Dip:
+                    newHealthValue = 1;
+                    break;
+                case EnemyName.Fly:
+                    newHealthValue = 1;
+                    break;
+                case EnemyName.Hopper:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.Pooter:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.Tado:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.Blib:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.WillOWisp:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.DigDig:
+                    newHealthValue = 1;
+                    break;
+                case EnemyName.Host:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.Longit:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.Imposter:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.MaskedImposter:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.BlueBoney:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.PurpleBoney:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.BoomFly:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.Larry:
+                    newHealthValue = 5;
+                    break;
+                case EnemyName.Burfer:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.GreenBlobby:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.Tader:
+                    newHealthValue = 8;
+                    break;
+                case EnemyName.CornyDip:
+                    newHealthValue = 1;
+                    break;
+                case EnemyName.Screecher:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.Sucker:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.Curser:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.Poofer:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.MegaPoofer:
+                    newHealthValue = 6;
+                    break;
+                case EnemyName.RedBlobby:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.BlackBlobby:
+                    newHealthValue = 5;
+                    break;
+                case EnemyName.Spookie:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.MirrorHauntLeft:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.MirrorHauntRight:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.Hanger:
+                    newHealthValue = 6;
+                    break;
+                case EnemyName.Isaacs:
+                    newHealthValue = 4;
+                    break;
+                case EnemyName.MeatGolem:
+                    newHealthValue = 7;
+                    break;
+                case EnemyName.FloatingCultist:
+                    newHealthValue = 5;
+                    break;
+                case EnemyName.WalkingCultist:
+                    newHealthValue = 5;
+                    break;
+                case EnemyName.Leechling:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.Greedling:
+                    newHealthValue = 3;
+                    break;
+                case EnemyName.RedCultist:
+                    newHealthValue = 5;
+                    break;
+                case EnemyName.Flipper:
+                    newHealthValue = 5;
+                    break;
+                case EnemyName.GreenBlib:
+                    newHealthValue = 2;
+                    break;
+                case EnemyName.ManaWisp:
+                    newHealthValue = 1;
+                    break;
+            }
+
+            if (newHealthValue != 0)
+            {
+                Console.WriteLine("[The Legend of Bum-bo: Windfall] Overriding starting health of newly spawned " + __instance.app.model.enemies[__instance.app.model.enemies.Count - 1].enemyName);
+                __instance.app.model.enemies[__instance.app.model.enemies.Count - 1].health = newHealthValue;
+            }
+        }
+
+        //Patch: Fixes a bug in the Tainted Peeper spawn blib logic; it now checks spaces on the y axis instead of the x axis
+        [HarmonyPatch(typeof(PeepsBoss), "SpawnBlib")]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var code = new List<CodeInstruction>(instructions);
+
+            bool finishedFirstField = false;
+            for (int i = 0; i < code.Count - 1; i++)
+            {
+                //Checking for a specific order of IL codes
+                if (code[i].opcode == OpCodes.Ldfld && code[i - 1].opcode == OpCodes.Ldfld && code[i + 1].opcode == OpCodes.Call)
+                {
+                    //Change operands from enemyposition.x to enemyposition.y
+                    var operand = AccessTools.Field(typeof(EnemyPosition), nameof(EnemyPosition.y));
+                    if (finishedFirstField == false)
+                    {
+                        finishedFirstField = true;
+                        code[i].operand = operand;
+                    }
+                    else
+                    {
+                        code[i].operand = operand;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("[The Legend of Bum-bo: Windfall] Correcting Tainted Peeper spawn blib logic");
+            return code;
+        }
+    }
+}
---
+++ Windfall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+
+namespace The_Legend_of_Bum_bo_Windfall
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInProcess("The Legend of Bum-Bo.exe")]
+    public class Windfall : BaseUnityPlugin
+    {
+        private const string modGUID = "org.bepinex.plugins.thelegendofbumbowindfall";
+        private const string modName = "The Legend of Bum-bo: Windfall";
+        private const string modVersion = "0.0.1.0";
+        private readonly Harmony harmony = new Harmony("org.bepinex.plugins.thelegendofbumbowindfall");
+        void Awake()
+        {
+            //Patching with harmony
+            harmony.PatchAll();
+            Console.WriteLine("[The Legend of Bum-bo: Windfall] Applying harmony patch");
+            EntityFixes.Awake();
+            CollectibleFixes.Awake();
+            InterfaceFixes.Awake();
+            OtherChanges.Awake();
+        }
+    }
+}
