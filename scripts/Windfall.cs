5b9034ed1c19ca56fefce2f33164d5689f2badc2
Jeffrey <jeff.adriaanse@gmail.com>
2024-11-12 16:15:07 -0800

    Update internal mod version

--- scripts/Windfall.cs
+++ scripts/Windfall.cs
@@ -11,7 +11,7 @@ namespace The_Legend_of_Bum_bo_Windfall
     {
         private const string modGUID = "org.bepinex.plugins.thelegendofbumbowindfall";
         private const string modName = "The Legend of Bum-bo: Windfall";
-        private const string modVersion = "1.2.0.0";
+        private const string modVersion = "1.3.0.0";
         public static readonly Harmony harmony = new Harmony("org.bepinex.plugins.thelegendofbumbowindfall");
 
         private void Awake()
