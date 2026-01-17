dd837d09860a11b0d937cd435c312c02c4978474
Jeffrey <jeff.adriaanse@gmail.com>
2026-01-17 14:04:02 -0800

    Added Spanish localization

--- The_Legend_of_Bum_bo_Windfall/scripts/TextFixes.cs
+++ The_Legend_of_Bum_bo_Windfall/scripts/TextFixes.cs
@@ -61,6 +61,10 @@ namespace The_Legend_of_Bum_bo_Windfall
             //Modify Chinese
             string chineseName = "Chinese";
             ModifyLanguage(LanguageSourceData.GetLanguageIndex(chineseName), GetLanguageTextModification(chineseName));
+
+            //Modify Spanish
+            string spanishName = "Spanish";
+            ModifyLanguage(LanguageSourceData.GetLanguageIndex(spanishName), GetLanguageTextModification(spanishName));
         }
 
         private static Dictionary<string, string> GetLanguageTextModification(string languageName)
@@ -192,120 +196,6 @@ namespace The_Legend_of_Bum_bo_Windfall
 
             string translation = termData.GetTranslation(lanugageIndex);
             return translation != null ? translation : string.Empty;
-        }
-
-        public static Dictionary<string, string> EnglishTextModifications
-        {
-            get
-            {
-                Dictionary<string, string> modifications = new Dictionary<string, string>
-                {
-                    //Spells
-                    { "Spells/EXORCISM_KIT_NAME", "Exorcism Kit"},
-                    { "Spells/MALLET_NAME", "Mallet"},
-                    { "Spells/SLEIGHT_OF_HAND_NAME", "Sleight of Hand"},
-                    { "Spells/TINY_DICE_NAME", "Tiny Dice"},
-
-                    //Spell descriptions
-                    { "Spells/BARBED_WIRE_DESCRIPTION", "Damage Attackers In Room"},
-                    { "Spells/BUMBO_SHAKE_DESCRIPTION", "Shuffles the Puzzle Board"},
-                    { "Spells/D4_DESCRIPTION", "Shuffles the Puzzle Board"},
-                    { "Spells/DARK_LOTUS_DESCRIPTION", "Gain 6 Random Mana"},
-                    { "Spells/DOG_TOOTH_DESCRIPTION", "Attack that Heals You"},
-                    { "Spells/EUTHANASIA_DESCRIPTION", "Hurts an Attacking Enemy"},
-                    { "Spells/PENTAGRAM_DESCRIPTION", "Gain +1 Spell Damage"},
-                    { "Spells/ROCK_DESCRIPTION", "Hits the Furthest Enemy"},
-                    { "Spells/ROCK_FRIENDS_DESCRIPTION", "Hits Enemies = to Spell Damage"},
-                    { "Spells/STICK_DESCRIPTION", "Whack Away!"},
-                    { "Spells/THE_VIRUS_DESCRIPTION", "Poisons Attacking Enemies"},
-                    { "Spells/YELLOW_BELT_DESCRIPTION", "+5% to Dodge Attacks"},
-
-                    //Spell GUI Notifications
-                    //{ "GUI Notifications/ATTACKERS_GET_HURT", "Attackers\nIn Room\nGet Hurt!" }, //Barbed Wire (unchanged)
-                    { "GUI Notifications/HURT_NEXT_ENEMY", "Hurt Next\nAttacking Enemy!" }, //Euthanasia
-                    { "GUI Notifications/GAINED_ITEM_DAMAGE", "Gained Spell Damage!" }, //Experimental, spell damage up
-                    { "GUI Notifications/SUPER_LIKELY_DODGE", "Super Likely\nTo Dodge\nNext Wave!" }, //Smoke Machine
-                    { "GUI Notifications/TRY_DODGE_ATTACK", "Bum-bo\nTry To Dodge\nAttacks!" }, //Yellow Belt
-
-                    //Trinkets
-                    { "Trinkets/CURVED_HORN_NAME", "Curved Horn"},
-                    { "Trinkets/DRAKULA_TEETH_NAME", "Dracula Teeth"},
-
-                    //Trinket descriptions
-                    { "Trinkets/BAG_O_SUCKING_DESCRIPTION", "Gain Mana when You Hit!"},
-                    { "Trinkets/GLITCH_DESCRIPTION", "What Will It Be?"},
-                    { "Trinkets/NINE_VOLT_DESCRIPTION", "Items May Gain Charges"},
-                    { "Trinkets/PINKY_DESCRIPTION", "May Gain Wilds on Kills"},
-                    { "Trinkets/RAINBOW_TICK_DESCRIPTION", "Reduces Spell Cost"},
-                    { "Trinkets/STRAY_BARB_DESCRIPTION", "Attackers May Take Damage"},
-                    { "Trinkets/THERMOS_DESCRIPTION", "Charge All Items + Heal"},
-
-                    //Gizzarda
-                    { "Bosses/GIZZARDA_TIP_2", "\"she's very resistant!\nplan ahead!\""},
-
-                    //Characters
-                    { "Characters/EMPTY_UNLOCK", "beat the game twice with the Brave, Nimble, Stout, Weird, and Dead."},
-
-                    //Unlocks
-                    { "Unlocks/BUMBO_SMASH", "BUM-BO SMASH"},
-                    { "Unlocks/BUMBO_THE_DEAD", "BUM-BO THE DEAD"},
-                    { "Unlocks/BUMBO_THE_EMPTY", "BUM-BO THE EMPTY"},
-                    { "Unlocks/BUMBO_THE_LOST", "BUM-BO THE LOST"},
-                    { "Unlocks/BUMBO_THE_NIMBLE", "BUM-BO THE NIMBLE"},
-                    { "Unlocks/BUMBO_THE_STOUT", "BUM-BO THE STOUT"},
-                    { "Unlocks/BUMBO_THE_WEIRD", "BUM-BO THE WEIRD"},
-                    { "Unlocks/NEEDLE", "NEEDLE"},
-                    { "Unlocks/STICK", "STICK"},
-                    { "Unlocks/TOOTHPICK", "TOOTHPICK"},
-                };
-                return modifications;
-            }
-        }
-
-        public static Dictionary<string, string> EnglishTextAdditions
-        {
-            get
-            {
-                Dictionary<string, string> additions = new Dictionary<string, string>
-                {
-                    //Spells
-                    { "Spells/MAGNIFYING_GLASS_NAME", "Magnifying Glass"},
-                    { "Spells/MAGNIFYING_GLASS_DESCRIPTION", "Enlarges a Random Tile"},
-                    { "Spells/PLASMA_BALL_NAME", "Plasma Ball"},
-                    { "Spells/PLASMA_BALL_DESCRIPTION", "Chain Attack"},
-                    { "Spells/READING_STONE_NAME", "Reading Stone"},
-                    { "Spells/READING_STONE_DESCRIPTION", "Enlarges a Tile"},
-
-                    //Trinkets
-                    { "Trinkets/COMPOST_BAG_NAME", "Compost Bag"},
-                    { "Trinkets/COMPOST_BAG_DESCRIPTION", "Moved Tile Becomes Wild!"},
-                    { "Trinkets/WISE_HIDDEN_NAME", "Wise's Ability"},
-                    { "Trinkets/WISE_HIDDEN_DESCRIPTION", "Compost Bag"},
-                    { "Trinkets/MILK_NAME", "Milk!"},
-                    { "Trinkets/MILK_DESCRIPTION", "Gain +1 Movement when Hurt!"},
-
-                    //GUI Notifications
-                    { "GUI Notifications/ENLARGE_TILE", "Pick Tile to Enlarge!"},
-                    { "GUI Notifications/NO_VIABLE_SPELLS", "No Viable Spells"},
-
-                    //Characters
-                    { "Characters/WISE_UNLOCK", "beat chapter 4."},
-
-                    //Unlocks
-                    { "Unlocks/EVERYTHING_IS_TERRIBLE_NEW", "EVERYTHING IS TERRIBLE!"},
-                    { "Unlocks/THE_GAME_IS_HARDER", "THE GAME IS HARDER!"},
-                    { "Unlocks/BUMBO_THE_WISE", "BUM-BO THE WISE"},
-                    { "Unlocks/PLASMA_BALL", "PLASMA BALL"},
-                    { "Unlocks/MAGNIFYING_GLASS", "MAGNIFYING GLASS"},
-                    { "Unlocks/COMPOST_BAG", "COMPOST BAG"},
-
-                    //Menu
-                    { "Menu/WINDFALL_OPTIONS", "Windfall Options"},
-                    { "Menu/MINI_CREDITS_ROLL", "Mini Credits Roll"},
-                };
-                return additions;
-            }
-
         }
     }
 }
